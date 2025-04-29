0757a279e1bdb373799ad6bcc859967a4a275f33
Omri Sarig <omri.sarig@prevas.dk>
2025-04-29 10:23:06 +0200

    omsa: Enable various local plugins

--- init.lua
+++ init.lua
@@ -1090,10 +1090,10 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.git',
   require 'kickstart.plugins.which-key',
   require 'kickstart.plugins.color',
